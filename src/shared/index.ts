a0ef0f7d828072277e6bb50d6a522086ff48afe0
Lucas Silveira <lugano1995@gmail.com>
2023-03-22 23:13:57 -0300

    feat: create shared domain objects

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,3 +1,4 @@
+export * as DomainObjects from './domain-objects';
 export * as InfraObjects from './infra-objects';
 export * as Telemetry from './telemetry';
 export * as NestAddons from './nest-addons';
