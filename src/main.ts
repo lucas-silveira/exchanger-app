8f2e36f1c9d79f221cb2a88a8832f3dc84b7741c
Lucas Silveira <lugano1995@gmail.com>
2023-03-22 23:21:35 -0300

    feat: add app middlewares

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
+import * as Nest from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as NestAddons from '@shared/nest-addons';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.useLogger(app.get(NestAddons.AppLogger));
+  app.useGlobalInterceptors(
+    new NestAddons.Interceptors.HttpRequestCamelCaseInterceptor(),
+    new NestAddons.Interceptors.HttpResponseSnakeCaseInterceptor(),
+  );
+  app.useGlobalPipes(
+    new Nest.ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
